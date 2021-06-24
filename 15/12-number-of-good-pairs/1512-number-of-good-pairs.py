905ce5813d8b72c65837da24a3991da415a22d6d
michedomingo <michedomingo@gmail.com>
2021-06-24 11:52:03 -0700

    1512 python solution

--- 15/12-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 15/12-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -6,24 +6,18 @@ class Solution:
         nums, freq = sorted(nums), {}
 
         for item in nums:
-            print("\n")
-            print(f"if item in freq | if {item} in {freq}")
             if item in freq:
-
-                print(f"freq[item] += 1 | {freq[item]} += 1")
                 freq[item] += 1
             else:
                 freq[item] = 1
-                print(f"else... freq[item] = 1 | {freq[item]} = 1")
 
-        print(freq)
-
-        answer = 0
+        goodPairs = 0
         for value in freq.values():
             if value != 0:
-                answer += (value**2 - value)//2
+                goodPairs += (value**2 - value)//2
 
-        print(f"Final answer: {answer}")
+        # print(goodPairs)
+        return goodPairs
 
 
 def main():
