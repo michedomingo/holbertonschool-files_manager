3a35e084da9d0783529f7a5a6bc7ca67c5cb23ec
michedomingo <michedomingo@gmail.com>
2021-07-12 22:17:33 -0700

    Wrong 0001 python solution

--- 00/01-two-sum/0001-two-sum.py
+++ 00/01-two-sum/0001-two-sum.py
@@ -3,4 +3,23 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        sum, answer = 0, []
+
+        for i in range(len(nums) - 1):
+            sum = nums[i] + nums[i+1]
+            if sum == target:
+                answer.append(i)
+                answer.append(i + 1)
+                break
+
+        return answer
+
+
+def main():
+
+    output = Solution().twoSum(nums=[3, 2, 3], target=6)
+    print(f"Output: {output}")
+
+
+if __name__ == '__main__':
+    main()
