a2c32f69851e1619e78e2ac10d267e215c5eee2b
michedomingo <michedomingo@gmail.com>
2021-07-20 21:30:13 -0700

    Add 0020 python solution

--- 00/20-valid-parentheses/0020-valid-parentheses.py
+++ 00/20-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {'(': ')', '{': '}', '[': ']'}
+        dict = {'(': ')', '{': '}', '[': ']'}
         stack = []
         for i in s:
-            if i in d:  # 1
+            if i in dict:  # 1
                 stack.append(i)
-            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+            elif len(stack) == 0 or dict[stack.pop()] != i:  # 2
                 return False
         return len(stack) == 0  # 3
+
+# 1. if left bracket (i) key is within dict, then append it to the stack
+# 2. else if right bracket and stack is empty w/ no left bracket match, OR, top of stack left bracket value doesn't match
+# 3. if stack is empty return True, otherwise False if still contains unmatched left bracket
 
 
 def main():
