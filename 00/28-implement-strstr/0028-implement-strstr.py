a15f41453a187af6b4fdba60f945b05a4db24fbf
michedomingo <michedomingo@gmail.com>
2021-08-05 23:18:47 -0700

    Update 0028 python solution format of comments

--- 00/28-implement-strstr/0028-implement-strstr.py
+++ 00/28-implement-strstr/0028-implement-strstr.py
@@ -4,24 +4,26 @@ class Solution:
         needle_length = len(needle)
 
         if needle_length == 0:
-            return 0                                                # 1
+            return 0                                                 # 1
         elif haystack_length < needle_length:
-            return -1                                               # 2
+            return -1                                                # 2
         else:
-            for i in range((haystack_length - needle_length) + 1):  # 3a
-                if haystack[i:i + needle_length] == needle:         # 3b
-                    return i                                        # 3c
-            return -1                                               # 4
+            for i in range((haystack_length - needle_length) + 1):   # 3a
+                if haystack[i:i + needle_length] == needle:          # 3b
+                    return i                                         # 3c
+            return -1                                                # 4
 
-# 1. given edge case, if needle is empty string, return 0
-# 2. needle can't be found if haystack is smaller than needle, return -1
 
-# 3a. traverse haystack until the 'needle_length'th last index
-# 3b. check each consective substring of needle_length in haystack to match needle string
-# 3c. return substring starting index (i)
+"""
+1. given edge case, if needle is empty string, return 0
+2. needle can't be found if haystack is smaller than needle, return -1
 
-# 4. return -1 if needle not found in haystack
+3a. traverse haystack until the 'needle_length'th last index
+3b. check each consective substring of needle_length in haystack to match needle string
+3c. return substring starting index (i)
 
+4. return -1 if needle not found in haystack
+"""
 #        needle : l l
 # needle_length : 2
 # iterator  (i) : 0 1 2
