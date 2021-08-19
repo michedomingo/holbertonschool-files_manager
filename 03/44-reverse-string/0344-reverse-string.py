5ea7d3b92b2d80040fd4604202dc39426dcbc766
michedomingo <michedomingo@gmail.com>
2021-08-18 23:43:30 -0700

    Update notes

--- 03/44-reverse-string/0344-reverse-string.py
+++ 03/44-reverse-string/0344-reverse-string.py
@@ -6,17 +6,30 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left, right = 0, len(s)-1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        left, right = 0, len(s)-1                   # 1
+        while left < right:                         # 2
+            s[left], s[right] = s[right], s[left]   # 3
+            left += 1                               # 4
+            right -= 1                              # 5
+
+        print(s)
+
+
+"""
+1.
+2.
+3.
+4.
+5
+"""
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
 
 
 def main():
 
-    work = Solution()
-    work.reverseString(s=["h", "e", "l", "l", "o"])
+    Solution().reverseString(s=["h", "e", "l", "l", "o"])
 
 
 if __name__ == '__main__':
