8a0a14dc7d06268f4d5fe0eed82bb6a727f826df
michedomingo <michedomingo@gmail.com>
2021-09-20 15:45:22 -0700

    0007 python solution

--- 00/07-reverse-integer/0007-reverse-integer.py
+++ 00/07-reverse-integer/0007-reverse-integer.py
@@ -4,36 +4,87 @@ class Solution:
         MAX_INT = 2**31 - 1  # 2147483647
         sum = 0
 
-        def get_last_digit(num, mod):
+        def get_last_digit(num, mod):         # 1b
             if num < 0:
                 return num % -mod
             return num % mod
 
         def divide(num, divider):
-            return int(num * 1.0 / divider)  # prevent incorrect floor division
+            return int(num * 1.0 / divider)   # 2b
 
-        while num:
-            pop = get_last_digit(num, 10)
-            num = divide(num, 10)
+        while num:                            # 1
+            pop = get_last_digit(num, 10)     # 1a
+            num = divide(num, 10)             # 2a
 
-            # sum must be within constraints -2**31 <= x <= 2**31 - 1 throughout calculation
-            if (sum < divide(MIN_INT, 10)) or (sum == divide(MIN_INT, 10) and pop < -8):
+            if (sum < divide(MIN_INT, 10)) or (sum == divide(MIN_INT, 10) and pop < -8):   # 3a
                 return 0
-            if (sum > divide(MAX_INT, 10)) or (sum == divide(MAX_INT, 10) and pop > 7):
+            if (sum > divide(MAX_INT, 10)) or (sum == divide(MAX_INT, 10) and pop > 7):    # 3b
                 return 0
-            sum = sum * 10 + pop
-            # from interview standpoint code sum check (MIN_INT <= sum <= MAX_INT) after calculation is incorrect
+            sum = sum * 10 + pop                                                           # 4
 
         return sum
 
 
 """
-1.
-2.
-3.
+1.  While num is valid
+1a. Get last digit of num
+1b. Modulo num by 10 or -10 to extract remainder
+2a. Divide num by 10, to access next digit to extract remainder
+2b. Prevent incorrect floor division by casting from float to int
+3.  sum must be within contraints -2**31 <= x <= 2**31 - 1 throughout calculation
+3a. return 0, if sum < -214748364 OR sum == -214748364 & pop < -8 
+3b. return 0, if sum > 214748364 OR sum == 214748364 & pop > 7
+4.  Reverse num with remainders shifting to front
+note: from interview standpoint code sum check (MIN_INT <= sum <= MAX_INT) after calculation is incorrect
 """
-# Input: nums=[2, 7, 11, 15], target=9
+# Input: x = 123
 # _______________________________________
+# step#1b
+#       123
+# pop = num % 10
+#  3
+# _______________________________________
+# step#2a
+#           123         10
+# num = int(num * 1.0 / divider)
+#  12       12.3
+# _______________________________________
+# step#4    num: 12    pop: 3    sum: 0
+#        0          3
+# sum = sum * 10 + pop
+#  3
+# _______________________________________
+# step#1b
+#       12
+# pop = num % 10
+#  2
+# _______________________________________
+# step#2a
+#           12          10
+# num = int(num * 1.0 / divider)
+#  1        1.2
+# _______________________________________
+# step#4    num: 1     pop: 2    sum: 3
+#        3          2
+# sum = sum * 10 + pop
+#  32
+# _______________________________________
+# step#1b
+#       1
+# pop = num % 10
+#  1
+# _______________________________________
+# step#2a
+#           1           10
+# num = int(num * 1.0 / divider)
+#  0        0.1
+# _______________________________________
+# step#4    num: 0     pop: 1    sum: 32
+#        32         1
+# sum = sum * 10 + pop
+# 321
+# _______________________________________
+# Output: 321
 
 assert(Solution().reverse(x=123) == 321)
 assert(Solution().reverse(x=-123) == -321)
